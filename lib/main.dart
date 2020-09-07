861e878ee4d6976abc66eced4af93e235edf18ef
Bibek Upreti <bibekupreti9813@gmail.com>
2020-09-08 00:35:10 +0545

    Learning Flutter

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main()  =>runApp(MaterialApp(
+  home: Home(),
+));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My first app"),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Icon(
+          Icons.airport_shuttle,
+          color: Colors.blue,
+          size: 100,
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Text("click"),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
+
+
